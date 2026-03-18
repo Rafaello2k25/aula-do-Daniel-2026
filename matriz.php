6ac4ff97987465b8ea02a14abc4af16fd7a45640
Rafael <20241foz0020031@estudantes.ifpr.edu.br>
2026-03-18 15:30:06 -0300

    Delete matriz.php

--- matriz.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pokemon = array("nome" => "Pikachu", "tipo" => "Elétrico");
-$pokemon2 = array("nome" => "Charizard", "tipo" => "Fogo");
-$pokemon3 = array("nome" => "Zoro", "tipo" => "Santoryu");
-
-$pokemons = array($pokemon, $pokemon2, $pokemon3);
-
-echo "Nome do segundo pokemon: " . $pokemons[1]["nome"] . "\n";
